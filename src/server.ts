41c65a31002c01625ac68fc9474e1f9b225d7b07
Axel NGuessan <nguessanaxel21@gmail.com>
2026-02-25 02:52:20 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [ "http://localhost:5173", "https://ton-domaine.com" ],
+  origin: [ "http://localhost:5173", "https://project-resident-evil.vercel.app" ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
